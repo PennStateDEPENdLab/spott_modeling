5d79c40a0d3093af42b5f530ad8c870a5b01b014
ruofanma <r28ma1@gmail.com>
2022-02-13 16:37:20 -0500

    Update sbatch_one_ID.bash

--- code/param_recovery/sbatch_one_ID.bash
+++ code/param_recovery/sbatch_one_ID.bash
@@ -6,15 +6,17 @@
 #SBATCH -n 8
 #SBATCH -t 2:00:00
 
+#export var=cond000011
+
+echo $ID
+
 #module use /proj/mnhallqlab/sw/modules
 #module load matlab/2021a
 
 #matlab  -nodisplay -r "run matlabtest.m"
 #/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -r fit_group_vba_ffx_RM
 
-export var=cond000011
-
-/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -r matlabtest
+#/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -r matlabtest
 
 #module load r
 #R CMD BATCH --no-save --no-restore RTest.R
