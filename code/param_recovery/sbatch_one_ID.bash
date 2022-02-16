fbe6152394b06884310cd1c70387a08c3dba58ea
ruofanma <r28ma1@gmail.com>
2022-02-15 20:31:51 -0500

    Update sbatch_one_ID.bash

--- code/param_recovery/sbatch_one_ID.bash
+++ code/param_recovery/sbatch_one_ID.bash
@@ -14,8 +14,8 @@
 module use /proj/mnhallqlab/sw/modules
 module load matlab/2021a
 
-#matlab  -nodisplay -r fit_group_vba_ffx_RM /proj/mnhallqlab/projects/spott_modeling/par_sim/vba_${subj}.out
-matlab  -nodisplay -r matlabtest | tee vba_${subj}.out
+matlab  -nodisplay -r fit_group_vba_ffx_RM | tee /proj/mnhallqlab/projects/spott_modeling/par_sim/vba_${subj}.out
+#matlab  -nodisplay -r matlabtest | tee vba_${subj}.out
 
 #/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -r fit_group_vba_ffx_RM
 #/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -r matlabtest
