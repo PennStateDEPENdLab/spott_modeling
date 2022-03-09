87366631a7bde0abec1b7f5a01154dddfce94a2a
ruofanma <r28ma1@gmail.com>
2022-03-09 14:04:02 -0500

    full IDs .txt file

--- code/param_recovery/gen_IDs.R
+++ code/param_recovery/gen_IDs.R
@@ -1,5 +1,7 @@
+setwd("~/Documents/GitHub/spott_modeling/code/param_recovery")
+
 sink("sim_IDs.txt")
-for(i in 61:62){
+for(i in 1:5940){
   cat(paste0("cond", str_pad(i, 6, pad = "0")))
   cat("\n")
 }
