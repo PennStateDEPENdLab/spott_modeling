4c34a689b5f231e6f8704631343d271b51ade238
ruofanma <r28ma1@gmail.com>
2022-03-21 08:22:00 -0400

    Update data_for_plotting.R

--- code/data_for_plotting.R
+++ code/data_for_plotting.R
@@ -25,7 +25,8 @@ sim_grid <- expand.grid(
 )
 
 # select data for testing
-data_ind <- c(1:150, 1900:2050, 4057:4107)
+# data_ind <- c(1:150, 1900:2050, 4057:4107)
+data_ind <- c(1:10)
 
 # Preapre data for plotting
 
