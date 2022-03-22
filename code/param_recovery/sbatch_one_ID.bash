f358876ee77bd3d48c4b4332b5288bf46d45c09e
ruofanma <r28ma1@gmail.com>
2022-03-22 16:05:59 -0400

    Update sbatch_one_ID.bash

--- code/param_recovery/sbatch_one_ID.bash
+++ code/param_recovery/sbatch_one_ID.bash
@@ -14,7 +14,8 @@
 module use /proj/mnhallqlab/sw/modules
 module load matlab/2021a
 
-matlab  -nodisplay -r fit_group_vba_ffx_RM 2>&1 | tee /proj/mnhallqlab/projects/spott_modeling/par_rec_exp/vba_log_output/vba_${subj}.out
+# tee command echos the output to the screen and writes it to a separate file. Taking it out to avoid redundant .out files.
+matlab  -nodisplay -r fit_group_vba_ffx_RM 2>&1 /proj/mnhallqlab/projects/spott_modeling/par_rec_exp/vba_log_output/vba_${subj}.out
 #matlab  -nodisplay -r matlabtest | tee vba_${subj}.out
 
 #/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -r fit_group_vba_ffx_RM
