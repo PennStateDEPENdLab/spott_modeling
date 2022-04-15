9ab33c49d5c80bb1a81ba49cace6d6ed2426c206
ruofanma <r28ma1@gmail.com>
2022-04-15 10:44:30 -0400

    Create empirical_plotting.R

---
+++ code/empirical_plotting.R
@@ -0,0 +1,9 @@
+# This script takes in the recovery results from empirical data and makes a plot
+
+library(GGally)
+
+pandaa_exp_recovery <- read.csv("~/Documents/Lab_DEPENd/MotivationalVigor_PIT/SPOTT/spott_modeling//outputs/vba_out/ffx/pandaa_vba_input/exp/pandaa_vba_input_exp_ffx_global_statistics.csv")
+
+personEstimates = select(pandaa_exp_recovery, ends_with("transformed"))
+
+ggpairs(personEstimates)
