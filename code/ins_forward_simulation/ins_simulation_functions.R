091fa9c9da68ad0e5e7c373cd69aa475871ce1fd
ruofanma <r28ma1@gmail.com>
2022-06-21 15:19:36 -0400

    Update ins_simulation_functions.R

--- code/ins_forward_simulation/ins_simulation_functions.R
+++ code/ins_forward_simulation/ins_simulation_functions.R
@@ -4,7 +4,8 @@
 # trials, time duration of trials, and time bins
 setup_task_environment <- function(model=NULL, prew=list(0.3, 0.3), n_trials=200, trial_ms=6000, bin_ms=50) {
   
-  prew <- lapply(prew, eval.parent, n=1) # evaluate all prew expressions
+  # prew <- lapply(prew, eval.parent, n=1) # evaluate all prew expressions
+  prew <- lapply(prew, eval, envir = parent.frame()) # RM for 6/21/22
   lens <- sapply(prew, length)
   stopifnot(length(unique(lens)) == 1L)
   prew <- do.call(cbind, prew) #make into a matrix
