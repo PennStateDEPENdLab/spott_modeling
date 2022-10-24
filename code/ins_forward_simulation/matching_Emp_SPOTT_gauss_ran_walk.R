8d5f6e03bb3272168578de72e48e6070e557f4c7
ruofanma <r28ma1@gmail.com>
2022-10-24 08:06:32 -0400

    Update matching_Emp_SPOTT_gauss_ran_walk.R

--- code/ins_forward_simulation/matching_Emp_SPOTT_gauss_ran_walk.R
+++ code/ins_forward_simulation/matching_Emp_SPOTT_gauss_ran_walk.R
@@ -5,4 +5,8 @@ setwd(repo_dir)
 insblock_001 <- read.csv("001_insblock.csv")
 insblock_002 <- read.csv("002_insblock.csv")
 
+#not sure which one is A which one is B yet, but assigning values for now
 insblock_temp <- insblock_001 %>% group_by(instrial) %>% summarize(prewardA = unique(prewardA), prewardB = unique(prewardB), respA = sum(key == "9("), respB = sum (key=="8*"))
+
+
+#Need to calculated ratio (and log) of prewardA/prewardB, respA/respB
