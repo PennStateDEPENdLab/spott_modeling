32fb86fbc6dd864dac2e13abf9484f7c9185157b
ruofanma <r28ma1@gmail.com>
2022-10-24 17:38:58 -0400

    Update matching_Emp_SPOTT_gauss_ran_walk.R

--- code/ins_forward_simulation/matching_Emp_SPOTT_gauss_ran_walk.R
+++ code/ins_forward_simulation/matching_Emp_SPOTT_gauss_ran_walk.R
@@ -1,12 +1,18 @@
 library(dplyr)
 
-repo_dir <- "/Users/maruofan/Documents/GitHub/spott_modeling/data/SPOTT_gauss_ran_walk/data"
+repo_dir <- "/Users/maruofan/Documents/GitHub/spott_modeling/data/SPOTT_gauss_ran_walk/data" #Desktop
+repo_dir <- "/Users/ruofanma/Documents/GitHub/spott_modeling/data/SPOTT_gauss_ran_walk/data" #laptop
 setwd(repo_dir)
 insblock_001 <- read.csv("001_insblock.csv")
 insblock_002 <- read.csv("002_insblock.csv")
 
-#not sure which one is A which one is B yet, but assigning values for now
-insblock_temp <- insblock_001 %>% group_by(instrial) %>% summarize(prewardA = unique(prewardA), prewardB = unique(prewardB), respA = sum(key == "9("), respB = sum (key=="8*"))
 
+insblock_temp <- insblock_001 %>% group_by(instrial) %>% summarize(prewardA = unique(prewardA), prewardB = unique(prewardB), respA = sum (key=="8*"), respB = sum(key == "9("))
+insblock_temp$p1_p2 <- insblock_temp$prewardA/insblock_temp$prewardB
+insblock_temp$n1_n2 <- insblock_temp$respA/insblock_temp$respB
+insblock_temp$log_p1_p2 <- log(insblock_temp$p1_p2)
+insblock_temp$log_n1_n2 <- log(insblock_temp$n1_n2)
 
-#Need to calculated ratio (and log) of prewardA/prewardB, respA/respB
+insblock_temp <- insblock_temp %>% filter(log_n1_n2 > -Inf & log_n1_n2 < Inf)
+summary(lm(log_n1_n2 ~ log_p1_p2, insblock_temp))
+
