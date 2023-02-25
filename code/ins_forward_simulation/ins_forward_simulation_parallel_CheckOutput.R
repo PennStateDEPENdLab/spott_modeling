5144f9c003936fb2684decde29da20916a95c655
ruofanma <r28ma1@gmail.com>
2023-02-25 16:03:41 -0500

    Update ins_forward_simulation_parallel_CheckOutput.R

--- code/ins_forward_simulation/ins_forward_simulation_parallel_CheckOutput.R
+++ code/ins_forward_simulation/ins_forward_simulation_parallel_CheckOutput.R
@@ -1,5 +1,5 @@
 
 
 outputs <- list.files("/nas/longleaf/home/maruofan/mnhallqlab/projects/spott_modeling/outputs/par_sim_exp_full")
-save(ggDF, file = "/nas/longleaf/home/maruofan/GitHub/spott_modeling/data/parallel_outputs.RData")
+save(outputs, file = "/nas/longleaf/home/maruofan/GitHub/spott_modeling/data/parallel_outputs.RData")
 
