885200374a5d28cc53cb9378d323586b967c179e
ruofanma <r28ma1@gmail.com>
2023-02-25 16:21:52 -0500

    Update ins_forward_simulation_parallel_CheckOutput.R

--- code/ins_forward_simulation/ins_forward_simulation_parallel_CheckOutput.R
+++ code/ins_forward_simulation/ins_forward_simulation_parallel_CheckOutput.R
@@ -1,5 +1,13 @@
-
+# this script checks which simulation conditions are missing, when the number of output files is fewer than expected
 
 outputs <- list.files("/nas/longleaf/home/maruofan/mnhallqlab/projects/spott_modeling/outputs/par_sim_exp_full")
 save(outputs, file = "/nas/longleaf/home/maruofan/GitHub/spott_modeling/data/parallel_outputs.RData")
 
+out_IDs <- str_pad(1:32400, 5, pad = "0")
+out_IDs <- paste0("cond", out_IDs)
+
+for(i in 1:32400){
+  if(!(out_IDs[i] %in% outputs)){
+    print(i)
+  }
+}
