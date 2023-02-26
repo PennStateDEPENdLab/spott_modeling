0398fccc7fa7384400a3e82b56b1ea7900798584
ruofanma <r28ma1@gmail.com>
2023-02-26 06:45:47 -0500

    updating recovery bash

--- code/param_recovery/gen_IDs.R
+++ code/param_recovery/gen_IDs.R
@@ -2,8 +2,8 @@ library(stringr)
 setwd("~/Documents/GitHub/spott_modeling/code/param_recovery")
 
 sink("sim_IDs.txt")
-for (i in 1:3240){
-  cat(paste0("cond", str_pad(i, 6, pad = "0")))
+for (i in 1:12){
+  cat(paste0("cond", str_pad(i, 5, pad = "0")))
   cat("\n")
 }
 sink()
--- code/param_recovery/sbatch_one_ID.bash
+++ code/param_recovery/sbatch_one_ID.bash
@@ -15,7 +15,7 @@ module use /proj/mnhallqlab/sw/modules
 module load matlab/2021a
 
 # tee command echos the output to the screen and writes it to a separate file. Taking it out to avoid redundant .out files.
-matlab  -nodisplay -r fit_group_vba_ffx_RM 2>&1 /proj/mnhallqlab/projects/spott_modeling/par_rec_exp/vba_log_output/vba_exp_20_${subj}.out
+matlab  -nodisplay -r fit_group_vba_ffx_RM 2>&1 /proj/mnhallqlab/projects/spott_modeling/outputs/vba_log_output/vba_exp_full_${subj}.out
 #matlab  -nodisplay -r matlabtest | tee vba_${subj}.out
 
 #/Applications/MATLAB_R2020b.app/bin/matlab -nodisplay -r fit_group_vba_ffx_RM
