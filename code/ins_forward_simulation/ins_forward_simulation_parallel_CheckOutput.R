7d55c27b38399247ea1f24e7568c220a94128dab
ruofanma <r28ma1@gmail.com>
2023-03-20 14:15:50 -0400

    Update ins_forward_simulation_parallel_CheckOutput.R

--- code/ins_forward_simulation/ins_forward_simulation_parallel_CheckOutput.R
+++ code/ins_forward_simulation/ins_forward_simulation_parallel_CheckOutput.R
@@ -1,7 +1,8 @@
 # this script checks which simulation conditions are missing, when the number of output files is fewer than expected
 
-outputs <- list.files("/nas/longleaf/home/maruofan/mnhallqlab/projects/spott_modeling/outputs/par_sim_exp_full")
-save(outputs, file = "/nas/longleaf/home/maruofan/GitHub/spott_modeling/data/parallel_outputs.RData")
+# outputs <- list.files("/nas/longleaf/home/maruofan/mnhallqlab/projects/spott_modeling/outputs/par_sim_exp_full")
+outputs <- list.files("/nas/longleaf/home/maruofan/mnhallqlab/projects/spott_modeling/outputs/par_vba_out_full_ffx")
+# save(outputs, file = "/nas/longleaf/home/maruofan/GitHub/spott_modeling/data/parallel_outputs.RData")
 # I just downloaded the "parallel_outputs.RData" file from longleaf to work locally 
 
 out_IDs <- str_pad(1:32400, 5, pad = "0")
