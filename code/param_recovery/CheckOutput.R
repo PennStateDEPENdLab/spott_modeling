5a62d27eaf195d40ba9da6668100ed78ee4da356
ruofanma <r28ma1@gmail.com>
2023-03-20 14:41:42 -0400

    Update CheckOutput.R

--- code/param_recovery/CheckOutput.R
+++ code/param_recovery/CheckOutput.R
@@ -2,7 +2,7 @@ vba_out_dir <- "/proj/mnhallqlab/projects/spott_modeling/outputs/par_vba_out_ful
 
 simIDs <- read.delim("/nas/longleaf/home/maruofan/GitHub/spott_modeling/code/param_recovery/sim_IDs.txt", header = FALSE)
 for(i in length(simIDs)){
-  if(file.exists(paste0(vba_out_dir, simIDs[i,], "/exp/", simIDs[1,], "_exp_ffx_global_statistics.csv"))){
+  if(file.exists(paste0(vba_out_dir, simIDs[i,], "/exp/", simIDs[i,], "_exp_ffx_global_statistics.csv"))){
     print(i)
   }
 }
