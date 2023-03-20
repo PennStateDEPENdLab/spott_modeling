9d58feba68cb11ae1b9eb5f4ed4eb777745943e5
ruofanma <r28ma1@gmail.com>
2023-03-20 14:45:30 -0400

    updates

--- code/param_recovery/gen_IDs.R
+++ code/param_recovery/gen_IDs.R
@@ -2,7 +2,7 @@ library(stringr)
 setwd("~/Documents/GitHub/spott_modeling/code/param_recovery")
 
 sink("sim_IDs.txt")
-for (i in 1:10){
+for (i in 1:32400){
   cat(paste0("cond", str_pad(i, 5, pad = "0")))
   cat("\n")
 }
